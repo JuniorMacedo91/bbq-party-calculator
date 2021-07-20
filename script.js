2dc47c950ae7b2b7069847f22a80073868b83949
Junior Macedo <80720405+JuniorMacedo91@users.noreply.github.com>
2021-07-19 23:53:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ calcBtn.addEventListener('click', function(){
     if(adultInput.value ===''|| kidsInput.value ===''|| timeInput.value ===''){
         alert('Preencha todos os campos')
     } else{
-        resultBox.innerHTML += `<div class="result-content meat"><p> ${calcMeat(timeInput)} Kg</p> <img src="./assets/ham-leg.png" alt=" food icon"></div>`; 
+        resultBox.innerHTML = `<div class="result-content meat"><p> ${calcMeat(timeInput).toFixed(1)} Kg</p> <img src="./assets/ham-leg.png" alt=" food icon"></div>`; 
         resultBox.innerHTML += `<div class="result-content beer"><p> ${calcBeer(timeInput)} L</p> <img src="./assets/beer.png" alt="beer icon"></div>`; 
         resultBox.innerHTML += `<div class="result-content drink"><p> ${calcDrink(timeInput)} L</p> <img src="./assets/softdrinks.png" alt="drinks icon"></div>`;
     }
@@ -16,7 +16,7 @@ calcBtn.addEventListener('click', function(){
 
 function calcMeat(timeInput){
     const meatPerFourH = 0.5;
-    const meatPermoreH = 0.75;
+    const meatPermoreH = 0.65;
 
     if(timeInput.value <= 4){
        return (meatPerFourH * adultInput.value) + (kidsInput.value * (meatPerFourH / 2));
@@ -27,8 +27,8 @@ function calcMeat(timeInput){
 }   
 
 function calcBeer(timeInput){
-    const beerPerFourH = 1.000;
-    const beerPermoreH = 1.200;
+    const beerPerFourH = 1.200;
+    const beerPermoreH = 2.000;
 
     if(timeInput.value <=4){
         return (beerPerFourH * adultInput.value)
@@ -42,8 +42,8 @@ function calcDrink(timeInput){
     const drinkPermoreH = 1.500;
 
     if(timeInput.value <=4){
-        return (drinkPerFourH * Number(adultInput.value) + Number(kidsInput.value))
+        return (drinkPerFourH * Number(adultInput.value) + Number(kidsInput.value) * drinkPerFourH / 2)
     } else{
-        return (drinkPermoreH * Number(adultInput.value) + Number(kidsInput.value))
+        return (drinkPermoreH * Number(adultInput.value) + Number(kidsInput.value) * drinkPermoreH / 2)
     }
 }
